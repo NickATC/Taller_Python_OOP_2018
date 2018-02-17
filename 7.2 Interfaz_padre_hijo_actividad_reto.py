60693fb48375b5a03fbe08d09354aeec4d77d4d2
Nicolás Táutiva <33504315+NickATC@users.noreply.github.com>
2018-02-16 23:24:37 -0500

    Update 7.2 Interfaz_padre_hijo_actividad_reto.py

--- 7.2 Interfaz_padre_hijo_actividad_reto.py
+++ 7.2 Interfaz_padre_hijo_actividad_reto.py
@@ -1,29 +1,19 @@
+# -*- coding: utf-8 -*-
+
 # Actividad y reto
+
 # Output:
 # De mi padre he heredado:
 # 2 atributos
-#
-# De mi madre he heredado:
-#2 atributos
-
-# -*- coding: utf-8 -*-
 
 
 class Padre:
     #Agregue 2 atributos
     pass
 
-class Madre:
-    #Agregue 2 atributos diferentes a los de Padre
-    pass
 
 class HijoPadre():
     def describir_herencia_padre():
         #imprima los dos atributos heredados de Padre
-        pass
-
-class HijoMadre():
-    def describir_herencia_madre():
-        #imprima los dos atributos heredados de Madre
         pass
 
